eec472f371fbec3e6305b938b19cfab1d8185e80
vitaliy <vitaliy60606@gmail.com>
2026-02-28 13:17:38 +0200

    fixed not selected chat modal

--- src/components/chat/ChatNotSelected.tsx
+++ src/components/chat/ChatNotSelected.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function ChatNotSelected() {
     return (
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-1 flex-col items-center justify-center'>
             <Image
                 src={"/lets-chat.png"}
                 width={512}
